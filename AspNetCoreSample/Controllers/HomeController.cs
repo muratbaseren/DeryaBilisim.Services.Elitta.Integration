96b04ae323931486f91962357bb34bb27a099d42
Murat Başeren <muratbaseren@gmail.com>
2020-10-20 00:44:50 +0300

    Sample aspnet core app is done

---
+++ AspNetCoreSample/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using AspNetCoreSample.Models;
+using DeryaBilisim.Services.Elitta.Integration.Standart;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace AspNetCoreSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ElittaService _elittaService;
+
+        public HomeController(ILogger<HomeController> logger, ElittaService elittaService)
+        {
+            _logger = logger;
+            _elittaService = elittaService;
+        }
+
+        public IActionResult Index()
+        {
+            var response = _elittaService.AccountProvider.CreateIndividualAccount(new IndividualAccountCreateModel { Email = "shv89744@bcaoo.com", Phone = "99996282846", Name = "name1", Surname = "surname1", Address = "address1" });
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
